646fae6678cb650be2d8c4e311d87d37992e6510
Anastasia <anastaisafedotova@gmail.com>
2021-05-06 14:44:58 +0300

    refactor: change project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputComponent } from './input/input.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { ShortLinkInputComponent } from './components/short-link-input/short-link-input.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    InputComponent
+    ShortLinkInputComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +20,5 @@ import { MatFormFieldModule } from "@angular/material/form-field";
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/components/short-link-input/short-link-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-short-link-input',
+  templateUrl: './short-link-input.component.html',
+  styleUrls: ['./short-link-input.component.css']
+})
+export class ShortLinkInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
