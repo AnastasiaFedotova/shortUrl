9e1fdd357403ec224f49a93ab7140fb7b99fca44
Anastasia <anastaisafedotova@gmail.com>
2021-05-07 10:16:59 +0300

    refactor: add interface folder

--- src/app/services/links-service.service.ts
+++ src/app/services/links-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Link } from '../link';
+import { Link } from './../interface/link';
 
 @Injectable({
   providedIn: 'root'
