e03e729d1e61d35625ac7f5918da246ae1704f5a
Nastassia Fiadotava <nastassia_fiadotava@epam.com>
2021-05-27 10:46:09 +0300

    refactor: update result payload

--- src/app/store/effects/auth.effects.ts
+++ src/app/store/effects/auth.effects.ts
@@ -13,7 +13,7 @@ export class AuthEffect {
     ofType<CheckAuth>(EAuthActions.CheckAuth),
     mergeMap(() => this.authService.checkSession()
       .pipe(
-        map(isAuth => ({ type: EAuthActions.CheckAuthSuccess, payload: isAuth ? true : false })),
+        map(isAuth => ({ type: EAuthActions.CheckAuthSuccess, payload: !!isAuth }))
       )),
     catchError(err => {
       return throwError(err);
