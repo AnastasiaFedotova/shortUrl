993edea98b70932040864b600792ca2c4a4dfdb3
Anastasia <anastaisafedotova@gmail.com>
2021-06-07 23:26:46 +0200

    refactor: update routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRegestrarionFormComponent } from './components/user-regestrarion-form/user-regestrarion-form.component';
 import { LogInFormComponent } from './components/log-in-form/log-in-form.component';
+import { ShortenerUrlComponent } from './components/shortener-url/shortener-url.component';
 import { LinksListComponent } from './components/links-list/links-list.component';
 import { LinksTagPageComponent } from './components/links-tag-page/links-tag-page.component';
 import { UsersGuard } from './guards/users.guard';
@@ -9,6 +10,7 @@ import { GuestGuard } from './guards/guest.guard';
 import { LinkPageComponent } from './components/link-page/link-page.component';
 
 const routes: Routes = [
+  { path: '', component: ShortenerUrlComponent },
   { path: 'registration', component: UserRegestrarionFormComponent, canActivate: [GuestGuard] },
   { path: 'logIn', component: LogInFormComponent, canActivate: [GuestGuard] },
   { path: 'linkList', component: LinksListComponent, canActivate: [UsersGuard] },
