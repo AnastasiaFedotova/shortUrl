55f664a1e257cd6573eed758f9aae86f35366650
Anastasia <anastaisafedotova@gmail.com>
2021-06-16 09:31:23 +0200

    feat: add new animation implementation

--- src/app/styles/animation/shortenerForm.ts
+++ src/app/styles/animation/shortenerForm.ts
@@ -1,20 +1,41 @@
-import { trigger, state, style, animate, transition, keyframes, query, animateChild, group } from '@angular/animations';
+import { trigger, state, style, animate, transition, keyframes, query, animateChild, group, AnimationStyleMetadata } from '@angular/animations';
 
-const animationSetting = '0.5s';
+const animationSetting: string = '0.5s';
+const indentTextEnd: string = '220px';
+const indentTextMiddle: string = '110px';
+const indentTextStart: string = '10px';
+
+function shiftIndent(start: number, end: number): Array<AnimationStyleMetadata> {
+  const minIndent = 5;
+  let styles = [];
+
+  if (start < end) {
+    for (let i = start; i < end; i += minIndent) {
+      styles.push(style({ textIndent: i }))
+    }
+  } else {
+    for (let i = start; i > end; i -= minIndent) {
+      styles.push(style({ textIndent: `${i}px` }))
+    }
+  }
+
+
+  return styles;
+}
 
 export default [
   trigger('positionform', [
-    state('left', style({
+    state('right', style({
       transform: 'translate(35%, 0)'
     })),
-    state('right', style({
+    state('left', style({
       transform: 'translate(-50%, 0)'
     })),
     state('middle', style({
       transform: 'translate(0, 0)'
     })),
 
-    transition('left => right', [
+    transition('right => left', [
       group([
         animate(animationSetting, keyframes([
           style({
@@ -27,7 +48,7 @@ export default [
       ])
     ]),
 
-    transition('right => left', [
+    transition('left => right', [
       group([
         animate(animationSetting, keyframes([
           style({
@@ -40,7 +61,7 @@ export default [
       ])
     ]),
 
-    transition('left => middle, right => middle', [
+    transition('right => middle, left => middle', [
       group([
         animate(animationSetting, keyframes([
           style({
@@ -53,7 +74,7 @@ export default [
       ])
     ]),
 
-    transition('middle => right', [
+    transition('middle => left', [
       group([
         animate(animationSetting, keyframes([
           style({
@@ -66,7 +87,7 @@ export default [
       ])
     ]),
 
-    transition('middle => left', [
+    transition('middle => right', [
       group([
         animate(animationSetting, keyframes([
           style({
@@ -81,56 +102,40 @@ export default [
   ]),
 
   trigger('positioninput', [
-    state('left', style({
+    state('right', style({
+      textIndent: indentTextEnd,
       textAlign: 'right'
     })),
-    state('right', style({
+    state('left', style({
+      textIndent: indentTextStart,
       textAlign: 'left'
     })),
     state('middle', style({
-      textAlign: 'center'
+      textIndent: indentTextMiddle
     })),
 
-    transition('right => left', [
-      animate(animationSetting, keyframes([
-        style({padding: '0 0 0 0'}),
-        style({padding: '0 0 0 40%'})
-      ]))
-    ]),
-
     transition('left => right', [
-      animate(animationSetting, keyframes([
-        style({padding: '0 15% 0 0'}),
-        style({padding: '0 60% 0 0'})
-      ]))
+      animate(animationSetting, keyframes(shiftIndent(10, 220)))
     ]),
 
-    transition('left => middle', [
-      animate(animationSetting, keyframes([
-        style({padding: '0 30% 0 0'}),
-        style({padding: '0 40% 0 0'})
-      ]))
-    ]),
-
-    transition('middle => left', [
-      animate(animationSetting, keyframes([
-        style({padding: '0 0 0 20%'}),
-        style({padding: '0 0 0 30%'})
-      ]))
-    ]),
-
-    transition('middle => right', [
-      animate(animationSetting, keyframes([
-        style({padding: '0 0 0 0'}),
-        style({padding: '0 30% 0 0'})
-      ]))
+    transition('right => left', [
+      animate(animationSetting, keyframes(shiftIndent(220, 10)))
     ]),
 
     transition('right => middle', [
-      animate(animationSetting, keyframes([
-        style({padding: '0 0 0 0'}),
-        style({padding: '0 0 0 20%'})
-      ]))
+      animate(animationSetting, keyframes(shiftIndent(220, 110)))
+    ]),
+
+    transition('middle => right', [
+      animate(animationSetting, keyframes(shiftIndent(110, 220)))
+    ]),
+
+    transition('middle => left', [
+      animate(animationSetting, keyframes(shiftIndent(110, 10)))
+    ]),
+
+    transition('left => middle', [
+      animate(animationSetting, keyframes(shiftIndent(10, 110)))
     ]),
   ])
 ]
